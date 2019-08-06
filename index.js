282afbd339333965eeac7966f7eeb65176afdf31
arskinner <arskinner@gmail.com>
2019-08-06 11:51:20 -0700

    Add jargon support

--- index.js
+++ index.js
@@ -8,11 +8,12 @@ var quote = require('quotation')
 var fillers = require('fillers')
 var hedges = require('hedges')
 var weasels = require('weasels')
+var jargons = require('jargon')
 var unique = require('arr-union')
 
 module.exports = intensify
 
-var list = unique([], fillers, hedges, weasels).sort()
+var list = unique([], fillers, hedges, weasels, jargons).sort()
 
 var source = 'retext-intensify'
 
@@ -20,6 +21,7 @@ var source = 'retext-intensify'
 var filler = 'filler'
 var hedge = 'hedge'
 var weasel = 'weasel'
+var jargon = 'jargon'
 
 // Reasons.
 var reason = {}
@@ -27,6 +29,7 @@ var reason = {}
 reason[filler] = 'it doesn’t add meaning'
 reason[weasel] = 'it’s vague or ambiguous'
 reason[hedge] = 'it lessens impact'
+reason[jargon] = 'could be perceived as too formal or complex'
 
 // Attacher.
 function intensify(options) {
@@ -40,22 +43,38 @@ function intensify(options) {
     search(tree, phrases, searcher)
 
     function searcher(match, index, parent, phrase) {
-      var type = weasel
+      var type = jargon
       var actual = toString(match)
       var message
 
-      if (weasels.indexOf(phrase) === -1) {
-        type = fillers.indexOf(phrase) === -1 ? hedge : filler
+      if jargons.indexOf(phrase) === -1) {
+        type = weasel
+
+        if (weasels.indexOf(phrase) === -1) {
+          type = fillers.indexOf(phrase) === -1 ? hedge : filler
+        }
       }
 
-      message = file.message(
-        'Don’t use ' + quote(actual, '`') + ', ' + reason[type],
-        {
-          start: position.start(match[0]),
-          end: position.end(match[match.length - 1])
-        },
-        [source, type].join(':')
-      )
+      if type == jargon {
+        message = file.message(
+          quote(actual, '`') + ' ' + reason[type],
+          {
+            start: position.start(match[0]),
+            end: position.end(match[match.length - 1])
+          },
+          [source, type].join(':')
+        )
+      }
+      else {
+        message = file.message(
+          'Don’t use ' + quote(actual, '`') + ', ' + reason[type],
+          {
+            start: position.start(match[0]),
+            end: position.end(match[match.length - 1])
+          },
+          [source, type].join(':')
+        )
+      }
 
       message.actual = actual
       message.expected = []
